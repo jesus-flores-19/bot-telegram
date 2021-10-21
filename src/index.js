a32162a0ff661d5c35b11a4448309a57f1635d57
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-20 23:28:51 -0500

    Intento de login

--- src/index.js
+++ src/index.js
@@ -1,45 +1,40 @@
-const Telegraf = require("telegraf").Telegraf;
+const { Telegraf, Markup } = require("telegraf");
+const loginjs = require("./logis");
 
-const bot = new Telegraf("2052372725:AAH7pPB14HfPuuv9PbRe52iUaLfzyQbiQ4Q");
-
+const bot = new Telegraf("2065795954:AAEwxaQybxKRIrWPqpFCO_iUYumKwi5_5xo");
+let sesion = false
 
 bot.start((ctx) => {
-    const mensaje = `<b>Bienvenido</b> al bot de prueba. \n\n¿En que puedo ayudarte, ${ctx.chat.first_name}?`
-    ctx.telegram.sendMessage(ctx.chat.id, mensaje, {
-        parse_mode: "HTML",
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: "Sin luz en pdv", callback_data: "problema_1" }, ],
-                [{ text: "Problema de red", callback_data: "problema_2" }, ],
-                [{ text: "Sin personal", callback_data: "problema_3" }, ]
-            ]
+    if (sesion) {
+        ctx.reply("huevos")
+    } else {
+        const mensaje = `<b>Bienvenido</b> al bot de prueba. \n\n¿Ya iniciaste sesión, ${ctx.chat.first_name}?`
+        ctx.telegram.sendMessage(ctx.chat.id, mensaje, {
+            parse_mode: "HTML",
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "Login", callback_data: "login" }, ],
+                ]
+            }
+        })
+    }
+})
+
+bot.action("login", (ctx) => {
+    ctx.answerCbQuery()
+    ctx.reply("Ingrese el usuario:")
+
+    bot.on("text", (ctx) => {
+        let mensaje = ctx.message.text.toLowerCase()
+        if (mensaje === "lol") {
+            ctx.reply("Bienvenido, pana. \n /start")
+        } else {
+            ctx.reply("Ingrese correctamente el usuario.")
         }
     })
 })
 
-bot.action("problema_1", (ctx) => {
-    ctx.reply("Los tecnicos van en camino")
-})
-bot.action("problema_2", (ctx) => {
-    const mensaje = `<b>Problema 2</b> \n\n¿Necesitas un tecnico, ${ctx.chat.first_name}?`
-    ctx.telegram.sendMessage(ctx.chat.id, mensaje, {
-        parse_mode: "HTML",
-        reply_markup: {
-            keyboard: [
-                [{ text: "Si", callback_data: "Si" }],
-                [{ text: "No" }]
-            ],
-            resize_keyboard: true,
-            one_time_keyboard: true
-        }
-    })
-
-})
-bot.hears("Si", (ctx) => {
-    ctx.reply("Se levanto un reporte, el técnico seria enviado")
-})
-
-bot.action("problema_3", (ctx) => {
-    ctx.reply("El reporte se ha levantado.\nEn unos minutos nos comunicaremos contigo.")
-})
+// bot.hears("Si", (ctx) => {
+//     ctx.reply("Se levanto un reporte, el técnico seria enviado")
+// })
 bot.launch()
---
+++ src/logis.js
@@ -0,0 +1,11 @@
+const Telegraf = require("telegraf").Telegraf;
+
+function login(ctx) {
+    ctx.reply("Se levanto un reporte, el técnico seria enviado")
+}
+
+
+
+module.exports = {
+    login
+}
